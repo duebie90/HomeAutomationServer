7bca3bed4c0f560551989e5af803c049b32bfac7
Manuel du Bois <mandubo@gmx.de>
2016-11-20 10:36:42 +0100

    Created WebUI Prototype: ClientSide-Code added.

---
+++ html/Hausautomation/w3js.js
@@ -0,0 +1,8 @@
+function w3_open() {
+    document.getElementById("sidenav").style.display = "block";
+    document.getElementById("masterheadline").style.marginLeft = "300px";
+}
+function w3_close() {
+    document.getElementById("sidenav").style.display = "none";
+    document.getElementById("masterheadline").style.marginLeft = "50px";
+}
