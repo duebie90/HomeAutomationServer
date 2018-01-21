95f11c0812eafeedce13ce0d70180e88b3c5c1f2
Manuel du Bois <Manuel.duBois@gmx.de>
2018-01-21 14:42:54 +0100

    Removed WebUI, as it got an own Repository

--- html/Hausautomation/w3js.js
+++
@@ -1,8 +0,0 @@
-function w3_open() {
-    document.getElementById("sidenav").style.display = "block";
-    document.getElementById("masterheadline").style.marginLeft = "300px";
-}
-function w3_close() {
-    document.getElementById("sidenav").style.display = "none";
-    document.getElementById("masterheadline").style.marginLeft = "50px";
-}
